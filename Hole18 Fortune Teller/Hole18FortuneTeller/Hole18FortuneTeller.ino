478906dbeea4c31c52d73c10e2c508d22b2068ba
Elliott Kember <elliott.kember@gmail.com>
2018-05-10 19:46:14 -0700

    Reformatted Fortune Teller

--- Hole18 Fortune Teller/Hole18FortuneTeller/Hole18FortuneTeller.ino
+++ Hole18 Fortune Teller/Hole18FortuneTeller/Hole18FortuneTeller.ino
@@ -11,7 +11,6 @@ AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
 AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;
 
-
 //Here is where you define which actions go with which pins.
 #define SDCARD_CS_PIN    10
 #define SDCARD_MOSI_PIN  7
@@ -39,62 +38,59 @@ AudioControlSGTL5000     sgtl5000_1;
 CRGB leds[NUM_LEDS_1];
 
 void setup() {
-    delay(3000); // sanity delay
-    FastLED.addLeds<APA102, ONdata, ONclock, COLOR_ORDER>(leds, NUM_LEDS_1).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(BRIGHTNESS);
-    
-    Serial.begin(9600);
-    AudioMemory(8);
-    sgtl5000_1.enable();
-    sgtl5000_1.volume(0.25);
-    SPI.setMOSI(SDCARD_MOSI_PIN);
-    SPI.setSCK(SDCARD_SCK_PIN);
-    if (!(SD.begin(SDCARD_CS_PIN))) {
+  delay(3000); // sanity delay
+
+  FastLED.addLeds<APA102, ONdata, ONclock, COLOR_ORDER>(leds, NUM_LEDS_1).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(BRIGHTNESS);
+
+  Serial.begin(9600);
+  AudioMemory(8);
+
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.25);
+  SPI.setMOSI(SDCARD_MOSI_PIN);
+  SPI.setSCK(SDCARD_SCK_PIN);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
     while (1) {
       Serial.println("Unable to access the SD card");
       delay(500);
     }
-      
-    
-    pinMode(IR_PIN, INPUT_PULLUP);
-    pinMode(specialFortune, OUTPUT);
-    pinMode(holeInOne, OUTPUT);
-    pinMode(buttonPin, INPUT_PULLUP);
+  }
 
-    delay(1000);
-}
+  pinMode(IR_PIN, INPUT_PULLUP);
+  pinMode(specialFortune, OUTPUT);
+  pinMode(holeInOne, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  delay(1000);
 }
 
 void loop() {
-  
-//  *******************
-//  *****MAIN CODE*****
-//  *******************
-int sense_motion = digitalRead(IR_PIN);
-int buttonPress = digitalRead(buttonPin);
+  int sense_motion = digitalRead(IR_PIN);
+  int buttonPress = digitalRead(buttonPin);
 
-Serial.println(sense_motion);
-    if(buttonPress == LOW) {
-      Serial.println("Start Wayfinder Script");
-      playSdWav1.play("HOLE_18_WAYFINDER_FORTUNE_TELLER.WAV");
-      delay(10);
-    }
-    if(sense_motion == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play("HOLE_18_BALL_TRIGGERED_FORTUNE_TELLER.WAV");
-          delay(10); // wait for library to parse WAV info
+  Serial.println(sense_motion);
 
-          digitalWrite(specialFortune, HIGH);
-          delay(10);
-          digitalWrite(specialFortune, LOW); //these 3 lines give the special fortune trigger
-          
-          for(int i = 0; i < 3; i++) {
-            digitalWrite(holeInOne, HIGH);
-            delay(500);
-            digitalWrite(holeInOne, LOW);
-            delay(500);
-          } //this for loop buzzes the booth light 3 times
-       }
+  if(buttonPress == LOW) {
+    Serial.println("Start Wayfinder Script");
+    playSdWav1.play("HOLE_18_WAYFINDER_FORTUNE_TELLER.WAV");
+    delay(10);
+  }
+
+  if(sense_motion == LOW) {
+    Serial.println("Start playing");
+    playSdWav1.play("HOLE_18_BALL_TRIGGERED_FORTUNE_TELLER.WAV");
+    delay(10); // wait for library to parse WAV info
+
+    digitalWrite(specialFortune, HIGH);
+    delay(10);
+    digitalWrite(specialFortune, LOW); //these 3 lines give the special fortune trigger
+
+    for(int i = 0; i < 3; i++) {
+      digitalWrite(holeInOne, HIGH);
+      delay(500);
+      digitalWrite(holeInOne, LOW);
+      delay(500);
+    } // this for loop buzzes the booth light 3 times
+  }
 }
-
-
