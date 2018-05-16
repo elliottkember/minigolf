fffea58949ac87d781f68916e97d54313c4df095
theperfectstraw <theperfectstraw@gmail.com>
2018-05-16 12:29:39 -0700

    Update Hole7DazzlingGold.ino
    Changed for only the Wayfinder script.

--- Hole7DazzlingGold/Hole7DazzlingGold.ino
+++ Hole7DazzlingGold/Hole7DazzlingGold.ino
@@ -11,12 +11,9 @@ AudioConnection          patchCord1(playSdWav1, 0, dacs1, 0);
 AudioConnection          patchCord2(playSdWav1, 1, dacs1, 1);
 // GUItool: end automatically generated code
 
-
-//Here is where you define which actions go with which pins.
 #define SDCARD_CS_PIN    BUILTIN_SDCARD
 
 #define buttonPin 18
-#define IR_Pin 19
 
 void setup() {
     Serial.begin(9600);
@@ -29,49 +26,18 @@ void setup() {
       }
     }
 
-    pinMode(buttonPin, INPUT_PULLUP);
-
-    int wayfinderButton = digitalRead(buttonPin);
-    int sense_motion = digitalRead(IR_Pin);
+    pinMode(buttonPin, INPUT_PULLUP); //Teensy pin for the Wayfinder Button
 
     delay(1000);
 }
 
 void loop() {
 
-wayfinderButton = digitalRead(buttonPin);
-sense_motion = digitalRead(IR_Pin);
+int wayfinderButton = digitalRead(buttonPin); //read the Wayfinder button.
 
     if(wayfinderButton == LOW) {
-          Serial.println("Start playing");
           playSdWav1.play("HOLE07.WAV"); //change the sound file here for each hole.
           delay(10); // wait for library to parse WAV info
     }
-
-    if(sense_motion == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play("DAZZLINGGOLD.WAV");
-          delay(10);
-    }
-}
-
-
-void loop() {
-
-int wayfinderButton = digitalRead(buttonPin);
-int sense_motion = digitalRead(triggerPin);
-Serial.println(wayfinderButton); //for testing purposes
-Serial.println(sense_motion); //for testing purposes
-
-    if(wayfinderButton == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play("HOLE_07_WAYFINDER_DAZZLING_GOLD.WAV"); //change the sound file here for each hole.
-          delay(10); // wait for library to parse WAV info
-    }
-
-    if(sense_motion == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play("HOLE_07_ BALL_TRIGGERED_DAZZLING_GOLD.WAV");
-          delay(10);
-    }
+    
 }
