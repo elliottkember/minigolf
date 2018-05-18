0aa648c3621aaa902b6f731a1dc4d2fa2d664089
theperfectstraw <theperfectstraw@gmail.com>
2018-05-18 10:44:25 -0700

    Update Hole17DragonsGate.ino
    Updated for actual code. Includes analog sound output for the roar, and some simple LED blinking for the eyes. We'll likely want something a bit more complicated on the LED pattern. This sketch will allow us to test everything on the Dragon.

--- Hole17DragonsGate/Hole17DragonsGate.ino
+++ Hole17DragonsGate/Hole17DragonsGate.ino
@@ -1,61 +1,77 @@
 #include <Audio.h>
 #include <Wire.h>
-#include <SD.h>
 #include <SPI.h>
+#include <SD.h>
 #include <SerialFlash.h>
+#include <FastLED.h>
 
-AudioPlaySdWav           playSdWav1;
-AudioOutputI2S           i2s1;
-AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
-AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;
+// GUItool: begin automatically generated code
+AudioPlaySdWav           playSdWav1;     //xy=105,182
+AudioOutputAnalogStereo  dacs1;          //xy=321,188
+AudioConnection          patchCord1(playSdWav1, 0, dacs1, 0);
+AudioConnection          patchCord2(playSdWav1, 1, dacs1, 1);
+// GUItool: end automatically generated code
+
+#define SDCARD_CS_PIN     BUILTIN_SDCARD
+#define COLOR_ORDER       BGR  //if your colors look incorrect, change the color order here
+#define NUM_LEDS          300  //change this number for the final LED count
+#define BRIGHTNESS        180
+#define FRAMES_PER_SECOND 20
+
+#define IR_Pin 18
+
+#define LEDdata  3
+#define LEDclock 4
 
 
-//Here is where you define which actions go with which pins.
-#define SDCARD_CS_PIN    10
-#define SDCARD_MOSI_PIN  7
-#define SDCARD_SCK_PIN 14
-
-#define buttonPin 18
-#define triggerPin 16
+CRGB leds[NUM_LEDS];
 
 void setup() {
+    //delay(3000); // sanity delay
+    FastLED.addLeds<SK9822, LEDdata, LEDclock, COLOR_ORDER, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+    FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000);
+    FastLED.setBrightness(BRIGHTNESS);
+    fill_solid(leds, NUM_LEDS, CRGB(255,147,41)); //fill eyes with warm white
+    FastLED.show();
+
     Serial.begin(9600);
-    AudioMemory(8);
-    sgtl5000_1.enable();
-    sgtl5000_1.volume(0.75);
-    SPI.setMOSI(SDCARD_MOSI_PIN);
-    SPI.setSCK(SDCARD_SCK_PIN);
+    AudioMemory(20);
     if (!(SD.begin(SDCARD_CS_PIN))) {
-    while (1) {
-      Serial.println("Unable to access the SD card");
-      delay(500);
+      // stop here, but print a message repetitively
+      while (1) {
+        Serial.println("Unable to access the SD card");
+        delay(500);
+      }
     }
-      
-    pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(triggerPin, INPUT_PULLUP);
-
     delay(1000);
-}
+
 }
 
+//  *******************
+//  *****MAIN LOOP*****
+//  *******************
 void loop() {
+  int senseMotion = digitalRead(IR_Pin);
 
-int wayfinderButton = digitalRead(buttonPin);
-int senseMotion = digitalRead(triggerPin);
-Serial.println(wayfinderButton); //for testing purposes
-Serial.println(senseMotion); //for testing purposes
+  if (senseMotion == LOW) {
+    Serial.println("Start playing");
+    playSdWav1.play("DRAGON17.WAV");
+    delay(10); // wait for library to parse WAV info
 
-    if(wayfinderButton == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play("HOLE_17_WAYFINDER_DRAGON_S_GATE.WAV"); //change the sound file here for each hole.
-          delay(10); // wait for library to parse WAV info
-     }
+    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
+    FastLED.delay(1000);
+    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
+    FastLED.show();
+  }
 
-     if(senseMotion == LOW) {
-      Serial.println("Motion Detected");
-      //still need to write the part of the sketch that flashes the eyes. Waiting on DA advice on the lights.
-      playSdWav1.play("HOLE_17_BALL_TRIGGERED_DRAGON_S GATE.WAV");
-      delay(10);
-     }
-}
+  }
