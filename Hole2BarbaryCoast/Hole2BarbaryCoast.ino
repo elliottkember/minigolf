9ee25edf76823ba1471152c15abd18d0caf454a0
theperfectstraw <theperfectstraw@gmail.com>
2018-05-23 12:06:04 -0700

    Update Hole2BarbaryCoast.ino

--- Hole2BarbaryCoast/Hole2BarbaryCoast.ino
+++ Hole2BarbaryCoast/Hole2BarbaryCoast.ino
@@ -1,50 +1,44 @@
 #include <Audio.h>
 #include <Wire.h>
-#include <SD.h>
 #include <SPI.h>
+#include <SD.h>
 #include <SerialFlash.h>
 
-AudioPlaySdWav           playSdWav1;
-AudioOutputI2S           i2s1;
-AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
-AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;
+// GUItool: begin automatically generated code
+AudioPlaySdWav           playSdWav1;     //xy=105,182
+AudioOutputAnalogStereo  dacs1;          //xy=321,188
+AudioConnection          patchCord1(playSdWav1, 0, dacs1, 0);
+AudioConnection          patchCord2(playSdWav1, 1, dacs1, 1);
+// GUItool: end automatically generated code
 
+#define SDCARD_CS_PIN BUILTIN_SDCARD
 
-//Here is where you define which actions go with which pins.
-#define SDCARD_CS_PIN    10
-#define SDCARD_MOSI_PIN  7
-#define SDCARD_SCK_PIN 14
-
-#define buttonPin 18
+#define buttonPin 19
 
 void setup() {
-    Serial.begin(9600);
-    AudioMemory(8);
-    sgtl5000_1.enable();
-    sgtl5000_1.volume(0.75);
-    SPI.setMOSI(SDCARD_MOSI_PIN);
-    SPI.setSCK(SDCARD_SCK_PIN);
-    if (!(SD.begin(SDCARD_CS_PIN))) {
+
+  Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  AudioMemory(20);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here, but print a message repetitively
     while (1) {
       Serial.println("Unable to access the SD card");
       delay(500);
     }
-      
-    pinMode(buttonPin, INPUT_PULLUP);
-
-    delay(1000);
-}
+  }
+  delay(1000);
 }
 
 void loop() {
+  int wayfinderButton = digitalRead(buttonPin);
 
-int wayfinderButton = digitalRead(buttonPin);
-Serial.println(wayfinderButton);
-
-    if(wayfinderButton == LOW) {
-          Serial.println("Start playing");
-          playSdWav1.play(HOLE_02_WAYFINDER_BARBARY_COAST.WAV"); //change the sound file here for each hole.
-          delay(10); // wait for library to parse WAV info
-       }
+  if (wayfinderButton == LOW) {
+    playSdWav1.play("HOLE02.WAV"); //change the audio file name to match the hole
+    delay(1000); // wait for library to parse WAV info
+  }
+  // do nothing while playing...
 }
+
