65274f8228edfb3f464c302938ff82e090859d60
Elliott Kember <elliott.kember@gmail.com>
2018-06-06 20:48:25 -0700

    First run at flashing eyes

--- Hole17DragonsGate/Hole17DragonsGate.ino
+++ Hole17DragonsGate/Hole17DragonsGate.ino
@@ -14,64 +14,62 @@ AudioConnection          patchCord2(playSdWav1, 1, dacs1, 1);
 
 #define SDCARD_CS_PIN     BUILTIN_SDCARD
 #define COLOR_ORDER       BGR  //if your colors look incorrect, change the color order here
-#define NUM_LEDS          300  //change this number for the final LED count
-#define BRIGHTNESS        180
+#define NUM_LEDS          97  //change this number for the final LED count
+#define BRIGHTNESS        255
 #define FRAMES_PER_SECOND 20
 
 #define IR_Pin 18
-
-#define LEDdata  3
+#define LEDdata 3
 #define LEDclock 4
-
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-    //delay(3000); // sanity delay
-    FastLED.addLeds<SK9822, LEDdata, LEDclock, COLOR_ORDER, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
-    FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000);
-    FastLED.setBrightness(BRIGHTNESS);
-    fill_solid(leds, NUM_LEDS, CRGB(255,147,41)); //fill eyes with warm white
-    FastLED.show();
+  //delay(3000); // sanity delay
+  pinMode(IR_Pin, INPUT_PULLUP);
+  FastLED.addLeds<SK9822, LEDdata, LEDclock, COLOR_ORDER, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000);
+  FastLED.setBrightness(BRIGHTNESS);
+  fill_solid(leds, NUM_LEDS, CRGB(255,147,41)); //fill eyes with warm white
+  FastLED.show();
 
-    Serial.begin(9600);
-    AudioMemory(20);
-    if (!(SD.begin(SDCARD_CS_PIN))) {
-      // stop here, but print a message repetitively
-      while (1) {
-        Serial.println("Unable to access the SD card");
-        delay(500);
-      }
+  Serial.begin(9600);
+  AudioMemory(20);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here, but print a message repetitively
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
     }
-    delay(1000);
-
+  }
+  delay(1000);
 }
 
-//  *******************
-//  *****MAIN LOOP*****
-//  *******************
+int hue = 0;
+
+void ambient() {
+  int brightness = beatsin16(30, 180, 200);
+  hue = beatsin16(20, 0, 32);
+  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, brightness));
+  FastLED.show();
+}
+
+void triggered() {
+  int brightness = beatsin16(60, 150, 255);
+  hue = beatsin16(80, 0, 100);
+  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, brightness));
+  FastLED.show();
+}
+
 void loop() {
   int senseMotion = digitalRead(IR_Pin);
-
+  ambient();
   if (senseMotion == LOW) {
     Serial.println("Start playing");
     playSdWav1.play("DRAGON17.WAV");
     delay(10); // wait for library to parse WAV info
-
-    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255,140,0)); //fill eyes with red
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
-    FastLED.delay(1000);
-    fill_solid(leds, NUM_LEDS, CRGB(255, 147,41));
-    FastLED.show();
+    while(playSdWav1.isPlaying()) {
+      triggered();
+    }
   }
-
-  }
+}
